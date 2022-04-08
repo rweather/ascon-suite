e44ef449ccd5c872bd42d5672c7f0cb8974ac023
Rhys Weatherley <rhys.weatherley@gmail.com>
2022-04-09 04:10:24 +1000

    Accelerate absorb and squeeze for hash algorithms

--- src/core/ascon-util-snp.h
+++ src/core/ascon-util-snp.h
@@ -36,24 +36,63 @@
 #include "ascon-util.h"
 #include <string.h>
 
-#if defined(xxxxASCON_BACKEND_SLICED32)
+#if defined(ASCON_BACKEND_SLICED32)
 
 #include "ascon-sliced32.h"
 
-// TODO
-
 #define ascon_separator(state) ((state)->W[8] ^= 0x01)
+#define ascon_pad(state, offset) \
+    ((state)->W[((offset) / 8) * 2 + 1] ^= \
+            (0x80000000U >> (((offset) & 7) * 4)))
 
-#elif defined(xxxxASCON_BACKEND_SLICED64)
+#define ascon_absorb_8(state, data) \
+    ascon_absorb_sliced((state), (data), 0)
+#define ascon_absorb_16(state, data) \
+    do { \
+        ascon_absorb_sliced((state), (data), 0); \
+        ascon_absorb_sliced((state), (data) + 8, 1); \
+    } while (0)
+#define ascon_absorb_partial(state, data, offset, count) \
+    ascon_add_bytes((state), (data), (offset), (count))
 
-// TODO
+#define ascon_squeeze_8(state, data) \
+    ascon_squeeze_sliced((state), (data), 0)
+#define ascon_squeeze_16(state, data) \
+    do { \
+        ascon_squeeze_sliced((state), (data), 0); \
+        ascon_squeeze_sliced((state), (data) + 8, 1); \
+    } while (0)
+#define ascon_squeeze_partial(state, data, offset, count) \
+    ascon_extract_bytes((state), (data), (offset), (count))
+
+#elif defined(ASCON_BACKEND_SLICED64)
 
 #define ascon_separator(state) ((state)->S[4] ^= 0x01)
 #define ascon_pad(state, offset) \
     ((state)->S[(offset) / 8] ^= \
             (0x8000000000000000ULL >> (((offset) & 7) * 8)))
 
-#elif defined(xxxxASCON_BACKEND_DIRECT_XOR)
+#define ascon_absorb_8(state, data) \
+    ((state)->S[0] ^= be_load_word64((data)))
+#define ascon_absorb_16(state, data) \
+    do { \
+        ((state)->S[0] ^= be_load_word64((data))); \
+        ((state)->S[1] ^= be_load_word64((data) + 8)); \
+    } while (0)
+#define ascon_absorb_partial(state, data, offset, count) \
+    ascon_add_bytes((state), (data), (offset), (count))
+
+#define ascon_squeeze_8(state, data) \
+    be_store_word64((data), (state)->S[0])
+#define ascon_squeeze_16(state, data) \
+    do { \
+        (be_store_word64((data), (state)->S[0])); \
+        (be_store_word64((data) + 8, (state)->S[1])); \
+    } while (0)
+#define ascon_squeeze_partial(state, data, offset, count) \
+    ascon_extract_bytes((state), (data), (offset), (count))
+
+#elif defined(ASCON_BACKEND_DIRECT_XOR)
 
 #define ascon_separator(state) ((state)->B[39] ^= 0x01)
 #define ascon_pad(state, offset) ((state)->B[(offset)] ^= 0x80)
