6dd02b350e4dad5468a78d5d4b427a68b016928c
Rhys Weatherley <rhys.weatherley@gmail.com>
2022-05-03 16:34:20 +1000

    Update docs

--- search/all_9.js
+++ search/all_9.js
@@ -8,6 +8,7 @@ var searchData=
   ['porting_2edox',['porting.dox',['../porting_8dox.html',1,'']]],
   ['porting_20the_20trng_20to_20new_20platforms',['Porting the TRNG to new platforms',['../porting_trng.html',1,'']]],
   ['posn',['posn',['../structascon128__state__t.html#ad6f17d652d56142ffaf44f79007b93b7',1,'ascon128_state_t::posn()'],['../structascon128a__state__t.html#a4abf606b195fa389f8302940f6830545',1,'ascon128a_state_t::posn()'],['../structascon80pq__state__t.html#a6014a33c99f356b848e3ea3da02e2846',1,'ascon80pq_state_t::posn()'],['../structascon__hkdf__state__t.html#a7f0012be4071c17225bac6d011ac29f6',1,'ascon_hkdf_state_t::posn()'],['../structascon__hkdfa__state__t.html#a1d0398394e101aba9e345e041f253809',1,'ascon_hkdfa_state_t::posn()'],['../structascon__trng__state__t.html#a21dc366b10f26349fa389f216e809e1e',1,'ascon_trng_state_t::posn()']]],
+  ['prf_2eh',['prf.h',['../prf_8h.html',1,'']]],
   ['prk',['prk',['../structascon__hkdf__state__t.html#a50973da6164b1ba02c9aae1fe23f238f',1,'ascon_hkdf_state_t::prk()'],['../structascon__hkdfa__state__t.html#a13f29531be6a41dd6da23b7ecdd40632',1,'ascon_hkdfa_state_t::prk()']]],
   ['prng',['prng',['../structascon__trng__state__t.html#a0303b084790ca727266c120c17720536',1,'ascon_trng_state_t']]]
 ];
